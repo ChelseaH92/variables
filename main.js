34a971689d11268c0acdb4ffa8631befb627ff7f
Chelsea Hatton <chelseahatton1992@gmail.com>
2023-08-29 13:30:53 -0400

    variables completed

---
+++ main.js
@@ -0,0 +1,60 @@
+/* console.log("Hello, world!");
+let myName = "Dwayne The Rock Johnson";
+console.log(myName);
+
+let catsName = "Edgar";
+const catColor = "orange"
+var catAge = 5
+
+let totalCost;
+let totalWithTax;
+
+let costPerItem = 2.50;
+let numberOfItems = 4;
+let taxRate = 0.06;
+
+totalCost = costPerItem * numberOfItems;
+totalWithTax = totalCost + (taxRate * totalCost);
+
+console.log(totalWithTax);
+
+let likesDogs = true;
+let allergicToDogs = false; 
+
+let firstName = "Dwayne";
+let lastName = "Johnson";
+
+let fullName = firstName + " " + lastName;
+
+let x = "2";
+let y = "4";
+let total = x + y; 
+*/
+
+
+
+let firstName = "Chelsea";
+let lastName = "Hatton - Soon to Be Hamlin"
+const age = "31";
+let likesTravel = true;
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(age);
+console.log(likesTravel); 
+
+
+let temperature = 86;
+let conditions = 'sunny';
+let sunriseTime = '7am';
+let sunsetTime = '8pm';
+
+/* 
+console.log(temperature);
+console.log(conditions);
+console.log(sunriseTime);
+console.log(sunsetTime);
+*/
+
+console.log(`The weather is ${conditions} and it feels like ${temperature} outside.`);
+console.log(`The sun rose at ${sunriseTime} this morning and will set at ${sunsetTime} this evening.`)
